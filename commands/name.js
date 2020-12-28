7a725ca3a63302904953375cbf520ef3d85a9ff9
asdbee <54086109+asdbee@users.noreply.github.com>
2020-12-28 10:37:21 -0500

    Create name.js

---
+++ commands/name.js
@@ -0,0 +1,15 @@
+const gc = require('../gc.js').get
+const config = require('../config.js')
+function getGC(id){
+    return gc.gcCache.find((g) => g.mainChat === id)
+}
+module.exports = {
+    name: 'name',
+    usage: '{prefix}name',
+    execute(bot,msg,args){
+        const getName = msg.content.slice(config.prefix.length+args[0].length).trim()
+        msg.channel.guild.channels.get(getGC(msg.channel.id).category).edit({name: getName}).then(
+            bot.createMessage(msg.channel.id,'`✓` Renamed to '+getName)
+        )
+    }
+}
