c03fbadc71c908c3cee34e6edf717d1372a12ff5
asdbee <54086109+asdbee@users.noreply.github.com>
2020-12-28 10:19:58 -0500

    Move gcCache, delete commands from main

--- server.js
+++ server.js
@@ -8,17 +8,16 @@ const bot = new Eris(config.token);
 const prefix = config.prefix
 bot.commands = new Eris.Collection();
 
-const gcCache = [
-  {mainChat: '', category: '', ownerID: ''}
-]
-module.exports = {gcCache: gcCache}
+const gcCache = gc.gcCache
+
+module.exports = {}
 
 bot.on('ready', () => {
     console.log('Bot updated successfully')
     gc.allGCInfo().then((data) => {
       data.forEach((g) => gcCache.push({mainChat: g.chatID, category: g.categoryID, ownerID: g.ownerID}))
     })
-
+    bot.editStatus('online', { name: 'group chats', type: 3})
     const commandFiles = fs.readdirSync(__dirname + '/commands').filter(file => file.endsWith('.js'));
     for (const file of commandFiles) {
 	    const command = require(__dirname + `/commands/${file}`)
@@ -39,29 +38,6 @@ bot.on('messageCreate', (msg) => {
     catch (error) {
       console.error(error)
       bot.createMessage(msg.channel.id,'Unable to execute command.')
-    }
-
-    if (msg.content.toLowerCase().startsWith(prefix+'add')){
-        if (msg.channel.parentID === config.ignoredCatagory) return;
-        const user = (args[1].replace(/[\\<>@#&!]/g, ""))
-        const brotherChannels = msg.channel.guild.channels.filter((c) => c.parentID === msg.channel.parentID)
-        Promise.all(brotherChannels.map((c) => c.editPermission(user,'117760','0','member','Added')))
-          .then(() => {
-            bot.createMessage(msg.channel.id,msg.author.username+' added <@'+user+'>')
-          })
-    }
-
-    if (msg.content.toLowerCase().startsWith(prefix+'remove')){
-        if (msg.channel.parentID === config.ignoredCatagory) return;
-        const user = (args[1].replace(/[\\<>@#&!]/g, ""))
-        if (msg.channel.guild.members.get(user).voiceState.channelID !== null){
-            msg.channel.guild.members.get(user).edit({channelID: null})
-        }
-        const brotherChannels = msg.channel.guild.channels.filter((c) => c.parentID === msg.channel.parentID)
-        Promise.all(brotherChannels.map((c) => c.editPermission(user,'0','117760','member','Added')))
-          .then(() => {
-            bot.createMessage(msg.channel.id,msg.author.username+' removed <@'+user+'>')
-          })
     }
 })
 
